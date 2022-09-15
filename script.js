6efd755086bf65e217aee6222a9284b94d64d6d3
Yishak Wondim <it.yisak.wondim@gmail.com>
2022-09-15 15:01:42 +0300

    varibales declared

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var display = document.querySelector('.main-display');
+var errorMessage = document.querySelector('.error-message');
+var trending = document.querySelector('.trending');
+
+
+
+
+function operate(){
+
+}
+
+
+
+
+
