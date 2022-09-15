6b8668561133b93dc82081cee77e470268a179b4
Yishak Wondim <it.yisak.wondim@gmail.com>
2022-09-15 16:00:36 +0300

    displaying numbers on the screen

--- script.js
+++ script.js
@@ -2,13 +2,22 @@
 var display = document.querySelector('.main-display');
 var errorMessage = document.querySelector('.error-message');
 var trending = document.querySelector('.trending');
+const numbers = document.querySelectorAll('.number');
 
-
-
-
-function operate(){
-
+function displayNumber(){
+    numbers.forEach((number) => {
+        number.addEventListener('click', (e) => { 
+            display.textContent.length <= 12 ?  
+            display.textContent = display.textContent + e.target.getAttribute('data-value'):
+            display.textContent = display.textContent;
+            errorMessage.textContent = 'maximum length'
+        });
+    })
 }
+
+
+displayNumber();
+
 
 
 
