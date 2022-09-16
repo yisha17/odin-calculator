73190f8842eeb99a4bd28cc7e02b0bec713f09de
Yishak Wondim <it.yisak.wondim@gmail.com>
2022-09-16 15:05:21 +0300

    moving to laptop

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
+var display = document.querySelector(".main-display");
+var errorMessage = document.querySelector(".error-message");
+var trending = document.querySelector(".trending");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const reset = document.querySelector(".reset");
 
-var display = document.querySelector('.main-display');
-var errorMessage = document.querySelector('.error-message');
-var trending = document.querySelector('.trending');
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator');
-const reset = document.querySelector('.reset');
-
-function displayNumber(){
-    display.textContent = '0'
-    numbers.forEach((number) => {
-        number.addEventListener('click', (e) => { 
-            display.textContent.length <= 12 ?
-            display.textContent == '0' ? display.textcontent = e.target.getAttribute('data-value'): 
-            display.textContent = display.textContent + e.target.getAttribute('data-value'):
-            errorMessage.textContent = 'maximum length';
-        });
-    })
-}
-
-
-function displayOperation(){
-    displayNumber();
-    operators.forEach((operator)=> {
-        operator.addEventListener('click', ()=>{
-            trending.textContent = display.textContent + trending.textContent;
-            display.textContent = '0'
-        })
+function displayNumber() {
+  numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+      console.log(display.textContent);
+      if (display.textContent.length <= 12) {
+        if (display.textContent == "0") {
+          console.log(e.target.getAttribute("data-value"));
+          display.textcontent = e.target.getAttribute("data-value");
+        } else {
+          display.textContent =
+            display.textContent + e.target.getAttribute("data-value");
+        }
+      } else {
+        errorMessage.textContent = "maximum length";
+      }
     });
-
-    reset.addEventListener('click', ()=>{
-        trending.textContent = '';
-        display.textContent = '0'
-    })
-   
+  });
 }
 
+
+
+function displayOperation() {
+  operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+      trending.textContent = display.textContent + trending.textContent;
+      display.textContent = "0";
+    });
+  });
+
+  reset.addEventListener("click", () => {
+    trending.textContent = "";
+    display.textContent = "0";
+  });
+}
 
 displayOperation();
 
-
-
-
-
-
+displayNumber();
