bdc9fdfde36bab97a41b40a0bc9c49053078b02d
Yishak Wondim <it.yisak.wondim@gmail.com>
2022-09-16 10:12:54 +0300

    display update

--- script.js
+++ script.js
@@ -3,20 +3,40 @@ var display = document.querySelector('.main-display');
 var errorMessage = document.querySelector('.error-message');
 var trending = document.querySelector('.trending');
 const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const reset = document.querySelector('.reset');
 
 function displayNumber(){
+    display.textContent = '0'
     numbers.forEach((number) => {
         number.addEventListener('click', (e) => { 
-            display.textContent.length <= 12 ?  
+            display.textContent.length <= 12 ?
+            display.textContent == '0' ? display.textcontent = e.target.getAttribute('data-value'): 
             display.textContent = display.textContent + e.target.getAttribute('data-value'):
-            display.textContent = display.textContent;
-            errorMessage.textContent = 'maximum length'
+            errorMessage.textContent = 'maximum length';
         });
     })
 }
 
 
-displayNumber();
+function displayOperation(){
+    displayNumber();
+    operators.forEach((operator)=> {
+        operator.addEventListener('click', ()=>{
+            trending.textContent = display.textContent + trending.textContent;
+            display.textContent = '0'
+        })
+    });
+
+    reset.addEventListener('click', ()=>{
+        trending.textContent = '';
+        display.textContent = '0'
+    })
+   
+}
+
+
+displayOperation();
 
 
 
